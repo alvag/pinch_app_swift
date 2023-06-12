ca6f04ff3a27bbd9868bca6df3f79a874c3611d3
Max Alva Garcia <alva85@gmail.com>
2023-06-12 06:35:00 -0500

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Max Alva on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
