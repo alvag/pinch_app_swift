275aa1eac0de6ac0bc820507c452ef6c378e3677
Max Alva Garcia <alva85@gmail.com>
2023-06-12 07:09:01 -0500

    Double tap gesture

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,17 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+  // MARK: - PROPERTIES
+  @State private var isAnimating: Bool = false
+  @State private var imageScale: CGFloat = 1
+
+
+  var body: some View {
+    
+    // MARK: - CONTENT
+    NavigationView {
+      ZStack {
+        // MARK: - PAGE IMAGE
+        Image("magazine-front-cover")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(10)
+          .padding()
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+          .opacity(isAnimating ? 1 : 0)
+          .scaleEffect(imageScale)
+          .onTapGesture(count: 2, perform: {
+            if imageScale == 1 {
+              withAnimation(.spring()) {
+                imageScale = 5
+              }
+            } else {
+              withAnimation(.spring()) {
+                imageScale = 1
+              }
+            }
+          })
+      }
+      .navigationTitle("Pinch & Zoom")
+      .navigationBarTitleDisplayMode(.inline)
+      .onAppear(perform: {
+        withAnimation(.linear(duration: 1)) {
+          isAnimating = true
         }
-        .padding()
+      })
     }
+    .navigationViewStyle(.stack)
+  }
 }
 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
